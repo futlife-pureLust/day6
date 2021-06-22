5d4d5b1035a6d0f0ac1ff38664b186d0f48da36d
futlife-pureLust <futlife.love.daimon@gmail.com>
2021-06-22 13:09:40 +0800

    6-10 使用空合併運算子

--- main.kt
+++ main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     var beverage = readLine()
-    // beverage = null
+ // beverage = null
 
     if (beverage != null) {
         beverage = beverage.capitalize()
@@ -8,5 +8,6 @@ fun main(args: Array<String>) {
         println("I can't do that without crashing - beverage was null!")
     }
 
-    println(beverage)
+    val beverageServed: String = beverage ?: "Buttered Ale"
+    println(beverageServed)
 }
