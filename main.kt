07dcb7747327ae963f1d6a774c86f8a2eba5f6a2
futlife-pureLust <futlife.love.daimon@gmail.com>
2021-06-22 12:42:48 +0800

    5-13 函數類型作為返回類型

--- main.kt
+++ main.kt
@@ -1,19 +1,17 @@
 fun main(args: Array<String>) {
-    runSimulation("Guyal", ::printConstructionCost) { playerName, numBulidings ->
+    runSimulation()
+}
+fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
+fun configureGreetingFunction(): (String) -> String{
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2018
-        println("Adding $numBulidings houses")
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-}
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit,
-                         greetingFunction: (String, Int) -> String)
-{
-    val numBuildings = (1..3).shuffled().last() //Randomly selects 1, 2, or 3
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-fun printConstructionCost(numBuildings: Int){
-    val cost = 500
-    println("construction cost: ${cost * numBuildings}")
 }
